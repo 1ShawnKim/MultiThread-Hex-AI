e34612918ceb53874eeefde91caacab0db4d22eb
1ShawnKim <shkim0131@gmail.com>
2023-06-05 12:00:30 -0500

    Initial Commit

---
+++ MultiThread Hex AI/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MultiThread Hex AI
+//
+//  Created by Shawn Kim on 6/5/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
